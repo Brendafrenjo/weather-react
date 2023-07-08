68355025fbb7ad97f940546a5882be53fd4162c6
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-07-08 23:28:10 +0300

    Added axios to display temperature

---
+++ src/Weather.js
@@ -0,0 +1,32 @@
+import React from "react";
+import axios from "axios";
+import { MutatingDots } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function displayTemperature(response) {
+    alert(
+      `The temperature in ${response.data.name} is ${Math.round(
+        response.data.main.temp
+      )}°C`
+    );
+  }
+
+  let apiKey = `49b631c45785fe73d2a88477803dea22`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios(apiUrl).then(displayTemperature);
+  return (
+    <div>
+      <p>Hello from {props.city}</p>
+      <MutatingDots
+        height="80"
+        width="80"
+        radius="9"
+        color="orange"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+    </div>
+  );
+}
