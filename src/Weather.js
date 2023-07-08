a5072bea8b48690296e7143be1bf3049d4284a05
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-07-08 23:37:26 +0300

    Changed loader color

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,7 @@ export default function Weather(props) {
         height="80"
         width="80"
         radius="9"
-        color="orange"
+        color="purple"
         ariaLabel="loading"
         wrapperStyle
         wrapperClass
